a40ed0818a449958627b74e4950d46ced5ab27cc
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-02-20 21:28:20 -0300

    create application.test

---
+++ test/application.test.ts
@@ -0,0 +1,16 @@
+import { checkout } from "../src/application";
+
+describe("", () => {
+  test("Deve fazer um pedido com 3 produtos", async function () {
+    const input = {
+      cpf: "987.654.321-00",
+      items: [
+        { idProduct: 1, quantity: 1 },
+        { idProduct: 2, quantity: 1 },
+        { idProduct: 3, quantity: 3 },
+      ],
+    };
+    const output = await checkout(input);
+    expect(output.total).toBe(6350);
+  });
+});
