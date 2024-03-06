5ef42473cea1a2961ec018d8d5fd099ff67f2721
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-06 19:26:08 -0300

    Freight Calculator test

---
+++ test/FreightCalculator.test.ts
@@ -0,0 +1,25 @@
+import FreightCalculator from "../src/FreightCalculator";
+
+describe("Freight Calculator test", () => {
+  it("Deve calcular o frete", () => {
+    const product = {
+      width: 100,
+      height: 30,
+      length: 10,
+      weight: 3,
+    };
+    const freight = FreightCalculator.calculate(product);
+    expect(freight).toBe(30);
+  });
+
+  it("Deve calcular o mínimo", () => {
+    const product = {
+      width: 10,
+      height: 10,
+      length: 10,
+      weight: 0.9,
+    };
+    const freight = FreightCalculator.calculate(product);
+    expect(freight).toBe(10);
+  });
+});
