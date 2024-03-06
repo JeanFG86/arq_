194699f866da981368b9d98bbc5b35be9198ee13
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-06 19:21:46 -0300

    coupon test

---
+++ test/Coupon.test.ts
@@ -0,0 +1,9 @@
+import Coupon from "../src/Coupon";
+
+describe("Coupon test", () => {
+  it("Deve testar o cupon", async () => {
+    const coupon = new Coupon("VALE20", 20, new Date("2024-03-30T10:00:00:00"));
+    expect(coupon.isExpired()).toBeFalsy();
+    expect(coupon.getDiscount(1000)).toBe(200);
+  });
+});
