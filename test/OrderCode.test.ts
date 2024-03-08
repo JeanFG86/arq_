d7d704cc06c06eb3afef3cdbfb6703247bc98070
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-08 11:43:37 -0300

    Não deve criar um código para o pedido se a sequence for negativa

--- test/OrderCode.test.ts
+++ test/OrderCode.test.ts
@@ -5,4 +5,8 @@ describe("Order code Test", () => {
     const orderCode = new OrderCode(new Date("2024-03-08T10:00:00"), 1);
     expect(orderCode.getValue()).toBe("202400000001");
   });
+
+  it("Não deve criar um código para o pedido se a sequence for negativa", () => {
+    expect(() => new OrderCode(new Date("2024-03-08T10:00:00"), -1)).toThrow(new Error("Invalid sequence"));
+  });
 });
