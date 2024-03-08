5bd1b6a275b4c706ff14fd68f39f984213a731bc
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-08 14:04:03 -0300

    create class cpf

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,6 +1,12 @@
+import Order from "../src/Order";
+
 describe("Order test", () => {
-  it("Deve criar um pedido", () => {
+  it("Deve criar um pedido vazio com CPF válido", () => {
     const order = new Order("987.654.321-00");
     expect(order.getTotal()).toBe(0);
+  });
+
+  it("Não deve criar um pedido com CPF inválido", () => {
+    expect(() => new Order("111.111.111-11")).toThrow("Invalid cpf");
   });
 });
