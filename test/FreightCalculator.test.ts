c43fdbbf8f8784b4cb5cd31978efd9fcb32b92a0
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-12 14:14:59 -0300

    use Product entity on FreightCalculator

--- test/FreightCalculator.test.ts
+++ test/FreightCalculator.test.ts
@@ -1,24 +1,15 @@
 import FreightCalculator from "../src/domain/entities/FreightCalculator";
+import Product from "../src/domain/entities/Product";
 
 describe("Freight Calculator test", () => {
   it("Deve calcular o frete", () => {
-    const product = {
-      width: 100,
-      height: 30,
-      length: 10,
-      weight: 3,
-    };
+    const product = new Product(1, "A", 1000, 100, 30, 10, 3);
     const freight = FreightCalculator.calculate(product);
     expect(freight).toBe(30);
   });
 
   it("Deve calcular o mínimo", () => {
-    const product = {
-      width: 10,
-      height: 10,
-      length: 10,
-      weight: 0.9,
-    };
+    const product = new Product(1, "A", 1000, 10, 10, 10, 0.9);
     const freight = FreightCalculator.calculate(product);
     expect(freight).toBe(10);
   });
