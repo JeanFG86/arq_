a4416ea915d6439afd643d32995afe468edccc7e
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-15 21:00:42 -0300

    main_queue

--- src/infra/queue/RabbitMQAdapter.ts
+++ src/infra/queue/RabbitMQAdapter.ts
@@ -9,7 +9,7 @@ export default class RabbitMQAdapter implements Queue {
   }
 
   async on(queueName: string, callback: Function): Promise<void> {
-    const channel = this.conection.createChannel();
+    const channel = await this.conection.createChannel();
     await channel.assertQueue(queueName, { durable: true });
     channel.consume(queueName, async function (msg: any) {
       const input = JSON.parse(msg.content.toString());
@@ -19,7 +19,7 @@ export default class RabbitMQAdapter implements Queue {
   }
 
   async publish(queueName: string, data: any): Promise<void> {
-    const channel = this.conection.createChannel();
+    const channel = await this.conection.createChannel();
     await channel.assertQueue(queueName, { durable: true });
     channel.sendToQueue(queueName, Buffer.from(JSON.stringify(data)));
   }
