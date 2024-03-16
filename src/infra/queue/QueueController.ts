4bc5e6733794c2fc5f955f3d283005055afc9f87
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-15 21:19:55 -0300

    Deve testar com a fila

--- src/infra/queue/QueueController.ts
+++ src/infra/queue/QueueController.ts
@@ -5,7 +5,6 @@ export default class QueueController {
   constructor(readonly queue: Queue, readonly checkout: Checkout) {
     queue.on("checkout", async function (input: any) {
       const output = await checkout.execute(input);
-      console.log(output);
     });
   }
 }
