2d736c62833a9096e3c5da50273e57d5d1386250
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-19 14:59:15 -0300

    create formatMoney

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,38 @@
 <script setup lang="ts">
-const products = [
+import { reactive } from "vue";
+
+const products = reactive([
   { idProduct: 1, description: "A", price: 1000 },
   { idProduct: 2, description: "B", price: 5000 },
   { idProduct: 3, description: "C", price: 30 },
-];
+]);
+const order = reactive({
+  total: 0,
+  items: [] as any,
+});
+const addItem = function (product: any) {
+  order.items.push({ idProduct: product.idProduct, price: product.price, quantity: 1 });
+};
+const getTotal = function () {
+  let total = 0;
+  for (const item of order.items) {
+    total += item.price;
+  }
+  return total;
+};
+const formatMoney = function (amount: number) {
+  return new Intl.NumberFormat("en-IN", { style: "currency", currency: "USD" }).format(amount);
+};
 </script>
 
 <template>
   <div class="title">Checkout</div>
   <div v-for="product in products">
     <span class="product-description">{{ product.description }}</span>
-    <span class="product-price">{{ product.price }}</span>
+    <span class="product-price">{{ formatMoney(product.price) }}</span>
+    <button class="product-add-button" @click="addItem(product)">add</button>
   </div>
+  <div class="total">{{ formatMoney(getTotal()) }}</div>
 </template>
 
 <style scoped></style>
