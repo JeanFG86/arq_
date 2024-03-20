4b02b644a28f1a83e5d329707f94c31e0320309f
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-20 17:13:18 -0300

    add test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,20 +6,33 @@ const products = reactive([
   { idProduct: 2, description: "B", price: 5000 },
   { idProduct: 3, description: "C", price: 30 },
 ]);
+
 const order = reactive({
   total: 0,
   items: [] as any,
 });
+
 const addItem = function (product: any) {
-  order.items.push({ idProduct: product.idProduct, price: product.price, quantity: 1 });
+  const existingItem = order.items.find((item: any) => item.idProduct === product.idProduct);
+  if (!existingItem) {
+    order.items.push({ idProduct: product.idProduct, price: product.price, quantity: 1 });
+  } else {
+    existingItem.quantity++;
+  }
 };
+
 const getTotal = function () {
   let total = 0;
   for (const item of order.items) {
-    total += item.price;
+    total += item.price * item.quantity;
   }
   return total;
 };
+
+const getProductById = function (idProduct: number) {
+  return products.find((product: any) => product.idProduct === idProduct);
+};
+
 const formatMoney = function (amount: number) {
   return new Intl.NumberFormat("en-IN", { style: "currency", currency: "USD" }).format(amount);
 };
@@ -33,6 +46,10 @@ const formatMoney = function (amount: number) {
     <button class="product-add-button" @click="addItem(product)">add</button>
   </div>
   <div class="total">{{ formatMoney(getTotal()) }}</div>
+  <div v-for="item in order.items">
+    <span class="item-description">{{ getProductById(item.idProduct)?.description }}</span>
+    <span class="item-quantity">{{ item.quantity }}</span>
+  </div>
 </template>
 
 <style scoped></style>
