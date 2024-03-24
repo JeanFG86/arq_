3b6872218d8c87bc0e2a98724e08e234abbfaad2
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-24 16:02:17 -0300

    Deve calcular o frete com distancia variável

--- backend/checkout/test/unit/FreightCalculator.test.ts
+++ backend/checkout/test/unit/FreightCalculator.test.ts
@@ -2,10 +2,17 @@ import FreightCalculator from "../../src/domain/entities/FreightCalculator";
 import Product from "../../src/domain/entities/Product";
 
 describe("Freight Calculator test", () => {
-  it("Deve calcular o frete", () => {
+  it("Deve calcular o frete com distancia padrao", () => {
     const product = new Product(1, "A", 1000, 100, 30, 10, 3);
     const freight = FreightCalculator.calculate(product);
     expect(freight).toBe(30);
+  });
+
+  it("Deve calcular o frete com distancia variável", () => {
+    const product = new Product(1, "A", 1000, 100, 30, 10, 3);
+    const distance = 748.2217780081631;
+    const freight = FreightCalculator.calculate(product, distance);
+    expect(freight).toBe(22.45);
   });
 
   it("Deve calcular o mínimo", () => {
