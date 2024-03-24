c42a58726a1df40064a1681fc83b291987baaf35
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-24 17:30:38 -0300

    Deve calcular o frete para um pedido com CEP de origem e destino usando db

--- backend/checkout/test/integration/CalculateFreight.test.ts
+++ backend/checkout/test/integration/CalculateFreight.test.ts
@@ -3,6 +3,7 @@ import CalculateFreight from "../../src/application/CalculateFreight";
 import PgPromiseConnection from "../../src/infra/database/PgPromiseConnection";
 import ZipcodeData from "../../src/domain/data/ZipcodeData";
 import Zipcode from "../../src/domain/entities/Zipcode";
+import ZipcodeDataDatabase from "../../src/infra/data/ZipcodeDataDatabase";
 
 describe("Calculate Freight", () => {
   it("Deve calcular o frete para um pedido sem CEP de origem e destino", async () => {
@@ -27,7 +28,7 @@ describe("Calculate Freight", () => {
     await connection.close();
   });
 
-  it("Deve calcular o frete para um pedido com CEP de origem e destino", async () => {
+  it("Deve calcular o frete para um pedido com CEP de origem e destino usando um fake", async () => {
     const connection = new PgPromiseConnection();
     const productData = new ProductDataDatabase(connection);
     const zipcodeData: ZipcodeData = {
@@ -40,6 +41,21 @@ describe("Calculate Freight", () => {
         }
       },
     };
+    const calculateFreight = new CalculateFreight(productData, zipcodeData);
+    const input = {
+      from: "22030060",
+      to: "88015600",
+      items: [{ idProduct: 1, quantity: 1 }],
+    };
+    const output = await calculateFreight.execute(input);
+    expect(output.total).toBe(22.45);
+    await connection.close();
+  });
+
+  it("Deve calcular o frete para um pedido com CEP de origem e destino usando db", async () => {
+    const connection = new PgPromiseConnection();
+    const productData = new ProductDataDatabase(connection);
+    const zipcodeData = new ZipcodeDataDatabase(connection);
     const calculateFreight = new CalculateFreight(productData, zipcodeData);
     const input = {
       from: "22030060",
