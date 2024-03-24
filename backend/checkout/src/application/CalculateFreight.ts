96f9eaba4d2a03a0d67a957ce2e24ef0402982ab
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-03-24 16:48:16 -0300

    Deve calcular o frete para um pedido com CEP de origem e destino

--- backend/checkout/src/application/CalculateFreight.ts
+++ backend/checkout/src/application/CalculateFreight.ts
@@ -1,15 +1,25 @@
 import FreightCalculator from "../domain/entities/FreightCalculator";
 import ProductData from "../domain/data/ProductData";
+import ZipcodeData from "../domain/data/ZipcodeData";
+import DistanceCalculator from "../domain/entities/DistanceCalculator";
 
 export default class CalculateFreight {
-  constructor(readonly productData: ProductData) {}
+  constructor(readonly productData: ProductData, readonly zipcodeData: ZipcodeData) {}
 
   async execute(input: Input): Promise<Output> {
+    let distance;
+    if (input.from && input.to) {
+      const from = await this.zipcodeData.get(input.from);
+      const to = await this.zipcodeData.get(input.to);
+      if (from && to) {
+        distance = DistanceCalculator.calculate(from.coord, to.coord);
+      }
+    }
     let total = 0;
     for (const item of input.items) {
       const product = await this.productData.getProduct(item.idProduct);
       if (product) {
-        total += FreightCalculator.calculate(product);
+        total += FreightCalculator.calculate(product, distance);
       }
     }
     return {
@@ -19,6 +29,8 @@ export default class CalculateFreight {
 }
 
 type Input = {
+  from?: string;
+  to?: string;
   items: { idProduct: number; quantity: number }[];
 };
 
