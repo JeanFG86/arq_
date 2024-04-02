c500aece256761670db337be4644edc65c64693d
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-04-02 19:37:16 -0300

    Deve calcular o frete para um pedido sem CEP de origem e destino

--- backend/freight/test/integration/CalculateFreight.test.ts
+++ backend/freight/test/integration/CalculateFreight.test.ts
@@ -25,28 +25,27 @@ describe("Calculate Freight", () => {
     await connection.close();
   });
 
-  it.skip("Deve calcular o frete para um pedido com CEP de origem e destino usando um fake", async () => {
-    // const connection = new PgPromiseConnection();
-    // const productData = new ProductDataDatabase(connection);
-    // const zipcodeData: ZipcodeData = {
-    //   async get(code: string): Promise<Zipcode | undefined> {
-    //     if (code === "22030060") {
-    //       return new Zipcode("22030060", "", "", -27.5945, -48.5477);
-    //     }
-    //     if (code == "88015600") {
-    //       return new Zipcode("88015600", "", "", -22.9129, -43.2003);
-    //     }
-    //   },
-    // };
-    // const calculateFreight = new CalculateFreight(productData, zipcodeData);
-    // const input = {
-    //   from: "22030060",
-    //   to: "88015600",
-    //   items: [{ idProduct: 1, quantity: 1 }],
-    // };
-    // const output = await calculateFreight.execute(input);
-    // expect(output.total).toBe(22.45);
-    // await connection.close();
+  it("Deve calcular o frete para um pedido com CEP de origem e destino usando um fake", async () => {
+    const connection = new PgPromiseConnection();
+    const zipcodeData: ZipcodeData = {
+      async get(code: string): Promise<Zipcode | undefined> {
+        if (code === "22030060") {
+          return new Zipcode("22030060", "", "", -27.5945, -48.5477);
+        }
+        if (code == "88015600") {
+          return new Zipcode("88015600", "", "", -22.9129, -43.2003);
+        }
+      },
+    };
+    const calculateFreight = new CalculateFreight(zipcodeData);
+    const input = {
+      from: "22030060",
+      to: "88015600",
+      items: [{ volume: 0.03, density: 100, quantity: 1 }],
+    };
+    const output = await calculateFreight.execute(input);
+    expect(output.total).toBe(22.45);
+    await connection.close();
   });
 
   it.skip("Deve calcular o frete para um pedido com CEP de origem e destino usando db", async () => {
