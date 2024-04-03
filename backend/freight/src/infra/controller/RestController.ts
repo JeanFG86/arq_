95677942c1d2e3c564498659feea918c843d0c77
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-04-02 21:28:05 -0300

    Deve calcular o frete

--- backend/freight/src/infra/controller/RestController.ts
+++ backend/freight/src/infra/controller/RestController.ts
@@ -1,15 +1,10 @@
-import Checkout from "../../application/Checkout";
+import CalculateFreight from "../../application/CalculateFreight";
 import HttpServer from "../http/HttpServer";
 
 export default class RestController {
-  constructor(readonly httpServer: HttpServer, readonly checkout: Checkout) {
-    httpServer.on("get", "/products", async function (params: any, body: any) {
-      const output = [{ idProduct: 4, description: "D", price: 1000 }];
-      return output;
-    });
-
-    httpServer.on("post", "/checkout", async function (params: any, body: any) {
-      const output = await checkout.execute(body);
+  constructor(readonly httpServer: HttpServer, readonly calculateFreight: CalculateFreight) {
+    httpServer.on("post", "/calculateFreight", async function (params: any, body: any) {
+      const output = await calculateFreight.execute(body);
       return output;
     });
   }
--- backend/freight/src/main_api.ts
+++ backend/freight/src/main_api.ts
@@ -1,8 +1,3 @@
-import express from "express";
-import ProductDataDatabase from "./infra/data/ProductDataDatabase";
-import CouponDataDatabase from "./infra/data/CouponDataDatabase";
-import OrderDataDatabase from "./infra/data/OrderDataDatabase";
-import Checkout from "./application/Checkout";
 import PgPromiseConnection from "./infra/database/PgPromiseConnection";
 import ExpressHttpServer from "./infra/http/ExpressHttpServer";
 import RestController from "./infra/controller/RestController";
@@ -11,11 +6,7 @@ import CalculateFreight from "./application/CalculateFreight";
 
 const connection = new PgPromiseConnection();
 const httpServer = new ExpressHttpServer();
-const productData = new ProductDataDatabase(connection);
-const couponData = new CouponDataDatabase(connection);
-const orderData = new OrderDataDatabase(connection);
 const zipcodeData = new ZipcodeDataDatabase(connection);
-const calculateFreight = new CalculateFreight(productData, zipcodeData);
-const checkout = new Checkout(productData, couponData, orderData, calculateFreight);
-new RestController(httpServer, checkout);
-httpServer.listen(3000);
+const calculateFreight = new CalculateFreight(zipcodeData);
+new RestController(httpServer, calculateFreight);
+httpServer.listen(3001);
