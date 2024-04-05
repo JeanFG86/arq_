00ffd08797ffafe6cf3c9f009e5a93b73a2bd20c
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-04-05 18:55:42 -0300

    create project catalog

---
+++ backend/catalog/test/unit/Product.test.ts
@@ -0,0 +1,13 @@
+import Product from "../../src/domain/entities/Product";
+
+describe("Product test", () => {
+  it("Deve calcular o volume do produto", () => {
+    const product = new Product(1, "A", 1000, 100, 30, 10, 3);
+    expect(product.getVolume()).toBe(0.03);
+  });
+
+  it("Deve calcular a densidade do produto", () => {
+    const product = new Product(1, "A", 1000, 100, 30, 10, 3);
+    expect(product.getDensity()).toBe(100);
+  });
+});
