e56a58165ba202c676b9db5645b8645bd30a1dab
Jean Francisco Grutzmann <jeanfg86@gmail.com>
2024-04-20 17:33:46 -0300

    apply queue

--- backend/checkout/src/main_api.ts
+++ backend/checkout/src/main_api.ts
@@ -8,6 +8,7 @@ import FreightGatewayHttp from "./infra/gateway/FreightGatewayHttp";
 import CatalogGatewayHttp from "./infra/gateway/CatalogGatewayHttp";
 import StockGatewayHttp from "./infra/gateway/StockGatewayHttp";
 import RabbitMQAdapter from "./infra/queue/RabbitMQAdapter";
+import QueueController from "./infra/queue/QueueController";
 
 async function main() {
   const connection = new PgPromiseConnection();
@@ -20,7 +21,8 @@ async function main() {
   const queue = new RabbitMQAdapter();
   await queue.connect();
   const checkout = new Checkout(catalgoGateway, couponData, orderData, freightGateway, stockGateway, queue);
-  new RestController(httpServer, checkout);
+  new RestController(httpServer, checkout, queue);
+  new QueueController(queue, checkout);
   httpServer.listen(3000);
 }
 
